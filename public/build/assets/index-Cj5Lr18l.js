c1c9b1198d4183643f2dc3ec54b44ab8abbb34ae
seideeX <johnxedricalejo07@gmail.com>
2025-11-30 15:11:25 +0800

    testest

--- public/build/assets/index-Cj5Lr18l.js
+++
@@ -1,1 +0,0 @@
-import{g as r,b as t}from"./app-m0UtTjfp.js";var a=t();const e=r(a);export{e as R,a as r};
--- public/build/assets/useAppUrl-CoFLKL1h.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-m0UtTjfp.js";const p=()=>{const r="http://localhost:8000";return t.useMemo(()=>r,[])};export{p as u};
