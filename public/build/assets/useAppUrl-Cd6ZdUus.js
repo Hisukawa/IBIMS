dbe5923fcdb74167b0e46f55e0b1aa94d7ce6470
seideeX <johnxedricalejo07@gmail.com>
2026-02-10 18:47:39 +0800

    cra minor

---
+++ public/build/assets/useAppUrl-Cd6ZdUus.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-DVhHKceu.js";const s=()=>{const r="https://ilagan-city.bims-ph.online/".replace(/\/$/,"");return t.useMemo(()=>r,[])};export{s as u};
--- resources/js/Components/CRAsteps/Step5/DisasterReadiness.jsx
+++ resources/js/Components/CRAsteps/Step5/DisasterReadiness.jsx
@@ -1,31 +1,33 @@
-import Accordion from '@/Components/Accordion'
-import React from 'react'
-import DistributionProcess from './DistributionProcess'
-import TraningsInventory from './TraningsInventory'
-import ResponsiveEuipInventory from './ResponsiveEuipInventory'
-import BdrrmcDirectory from './BdrrmcDirectory'
-import BarangayEvacuationPlan from './BarangayEvacuationPlan'
+import Accordion from "@/Components/Accordion";
+import React from "react";
+import DistributionProcess from "./DistributionProcess";
+import TraningsInventory from "./TraningsInventory";
+import ResponsiveEuipInventory from "./ResponsiveEuipInventory";
+import BdrrmcDirectory from "./BdrrmcDirectory";
+import BarangayEvacuationPlan from "./BarangayEvacuationPlan";
 const DisasterReadiness = () => {
     return (
         <div>
             <Accordion title="11. Distribution of Relief Goods to Affected Families and Individuals">
                 <DistributionProcess />
             </Accordion>
-            <Accordion title="12. Inventory of Tranings Attended by the Members of BDRRMC">
+            <Accordion title="12. Inventory of Trainings Attended by the Members of BDRRMC">
                 <TraningsInventory />
             </Accordion>
-            <Accordion title="13. Inventory of Responsive Equipment that can be be utilized during Calamities
-            and Disasters">
+            <Accordion
+                title="13. Inventory of Responsive Equipment that can be utilized during Calamities
+            and Disasters"
+            >
                 <ResponsiveEuipInventory />
             </Accordion>
             <Accordion title="BDRRMC Directory">
                 <BdrrmcDirectory />
             </Accordion>
-            <Accordion title="BARANGAY EVACUATION PLAN">
+            <Accordion title="Barangay Evacuation Plan">
                 <BarangayEvacuationPlan />
             </Accordion>
         </div>
-    )
-}
+    );
+};
 
-export default DisasterReadiness
+export default DisasterReadiness;
